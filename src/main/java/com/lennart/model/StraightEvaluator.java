27201a7d7577940c24b762ea0815057bfbcf1a15
Lennart Popma <lennart.popma@ordina.nl>
2016-08-09 17:22:01 +0200

    Finished straight / straightdraw functions and created StraightEvaluator class

--- src/main/java/com/lennart/model/StraightEvaluator.java
+++ src/main/java/com/lennart/model/StraightEvaluator.java
@@ -1,120 +1,18 @@
 package com.lennart.model;
 
-import org.apache.commons.lang3.StringUtils;
-import org.springframework.util.CollectionUtils;
-
 import java.util.*;
 
 /**
- * Created by LPO10346 on 21-6-2016.
+ * Created by LPO10346 on 8/9/2016.
  */
-public class BoardEvaluator {
-    public static boolean isBoardRainbow(List<Card> board) {
-        if(getNumberOfSuitedCardsOnBoard(board) == 0) {
-            return true;
-        }
-        return false;
-    }
-
-    public static boolean hasBoardTwoOfOneSuit(List<Card> board) {
-        if(getNumberOfSuitedCardsOnBoard(board) == 2) {
-            return true;
-        }
-        return false;
-    }
-
-    public static boolean hasBoardThreeOfOneSuit(List<Card> board) {
-        if(getNumberOfSuitedCardsOnBoard(board) == 3 && (!isBoardSuited(board))) {
-            return true;
-        }
-        return false;
-    }
-
-    public static boolean hasBoardFourOfOneSuit(List<Card> board) {
-        if(getNumberOfSuitedCardsOnBoard(board) == 4 && (!isBoardSuited(board))) {
-            return true;
-        }
-        return false;
-    }
-
-    public static boolean isBoardSuited(List<Card> board) {
-        if(getNumberOfSuitedCardsOnBoard(board) == board.size()) {
-            return true;
-        }
-        return false;
-    }
-
-    public static boolean isBoardConnected(List<Card> board) {
-        List<Integer> boardRanks = getSortedCardRanksFromCardList(board);
-
-        if(boardContainsAce(board)) {
-            Integer aceLow = 1;
-            Integer aceHigh = 14;
-            boardRanks.add(aceLow);
-            boardRanks.remove(aceHigh);
-            Collections.sort(boardRanks);
-            int counter = 0;
-            for(int i = 0; i < (boardRanks.size()-1); i++) {
-                if(boardRanks.get(i) + 1 == boardRanks.get(i+1)) {
-                    counter++;
-                    if(counter == boardRanks.size()-1) {
-                        return true;
-                    }
-                    continue;
-                }
-                else {
-                    boardRanks.remove(aceLow);
-                    boardRanks.add(aceHigh);
-                    break;
-                }
-            }
-        }
-
-        for(int i = 0; i < (boardRanks.size()-1); i++) {
-            if(boardRanks.get(i) + 1 == boardRanks.get(i+1)) {
-                continue;
-            }
-            else {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    public static boolean hasBoardTwoConnectingCards(List<Card> board) {
-        List<Integer> boardRanks = getSortedCardRanksFromCardList(board);
-        int x = 0;
-        for(int i = 0; i < (boardRanks.size()-1); i++) {
-            if(boardRanks.get(i) + 1 == boardRanks.get(i+1)) {
-                x++;
-            }
-        }
-        if(x == 1) {
-            return true;
-        }
-        return false;
-    }
-
-    public static boolean isBoardPairedOnce(List<Card> board) {
-        if(getNumberOfPairsOnBoard(board) == 1) {
-            return true;
-        }
-        return false;
-    }
-
-    public static boolean isBoardPairedTwice(List<Card> board) {
-        if(getNumberOfPairsOnBoard(board) == 2) {
-            return true;
-        }
-        return false;
-    }
+public class StraightEvaluator extends BoardEvaluator {
 
     public static List<List<Integer>> getCombosThatMakeStraight(List<Card> board) {
         List<Integer> boardRanks = getSortedCardRanksFromCardList(board);
 
         boardRanks = removeDoubleEntriesInList(boardRanks);
 
-        Map <Integer, List<List<Integer>>> listsOfFoundCombos = new TreeMap();
+        Map<Integer, List<List<Integer>>> listsOfFoundCombos = new TreeMap();
         for(int i = 1; i <= 7; i++) {
             listsOfFoundCombos.put(i, new ArrayList<>());
         }
@@ -499,65 +397,6 @@ public class BoardEvaluator {
         return backdoorCombos;
     }
 
-    private static int getNumberOfSuitedCardsOnBoard(List<Card> board) {
-        StringBuilder s = new StringBuilder();
-        int x = 0;
-        for(Card c : board) {
-            s.append(c.getSuit());
-        }
-        for(int i = 0; i <= (s.length()-1); i++) {
-            int a = StringUtils.countMatches(s, "" + s.charAt(i));
-            if(a > 1 && a > x) {
-                x = a;
-            }
-        }
-        return x;
-    }
-
-    private static int getNumberOfPairsOnBoard(List<Card> board) {
-        List<Integer> boardRanks = getSortedCardRanksFromCardList(board);
-        int x = 0;
-        int y = 0;
-        for(int i : boardRanks) {
-            if(Collections.frequency(boardRanks, i) == 2 && i != y) {
-                x++;
-                y = i;
-            }
-        }
-        return x;
-    }
-
-    private static List<Integer> getSortedCardRanksFromCardList(List<Card> board) {
-        List<Integer> boardRanks = new ArrayList<Integer>();
-        for(Card c : board) {
-            boardRanks.add(c.getRank());
-        }
-        Collections.sort(boardRanks);
-        return boardRanks;
-    }
-
-    private static boolean boardContainsAceAndWheelCard(List<Card> board) {
-        List<Integer> boardRanks = getSortedCardRanksFromCardList(board);
-        Integer a = 2;
-        Integer b = 3;
-        Integer c = 4;
-        Integer d = 5;
-        Integer ace = 14;
-
-        List<Integer> wheelCards = new ArrayList<Integer>();
-        wheelCards.add(a);
-        wheelCards.add(b);
-        wheelCards.add(c);
-        wheelCards.add(d);
-
-        if(boardRanks.contains(ace)) {
-            if (CollectionUtils.containsAny(boardRanks, wheelCards)) {
-                return true;
-            }
-        }
-        return false;
-    }
-
     public static Map<List<Integer>, List<List<Integer>>> getCombosThatGiveAnyStraightDraw(List<Card> board) {
         List<Integer> boardRanks = getSortedCardRanksFromCardList(board);
         Map<Integer, List<Integer>> allCardCombos = getAllPossibleCombos();
@@ -833,47 +672,6 @@ public class BoardEvaluator {
         return allPossibleStraights;
     }
 
-
-    private static Map<Integer, List<Integer>> getAllPossibleCombos() {
-        Map<Integer, List<Integer>> allPossibleCombos = new HashMap<>();
-        int counter = 0;
-        for(int i = 2; i < 15; i++) {
-            List<Integer> combo = new ArrayList<>();
-            combo.add(i);
-            for (int j = 14; j >= i; j--) {
-                combo.add(j);
-                List<Integer> comboCopy = new ArrayList<>();
-                comboCopy.addAll(combo);
-                allPossibleCombos.put(counter, comboCopy);
-                combo.remove((Integer) j);
-                counter++;
-            }
-        }
-        return allPossibleCombos;
-    }
-
-    private static <E> List<E> getDoubleEntriesFromList(List<E> list) {
-        Set<E> hs1 = new HashSet<E>();
-        Set<E> hs2 = new HashSet<E>();
-        List<E> listToReturn = new ArrayList<>();
-
-        for(E e : list) {
-            if(!hs1.add(e)) {
-                hs2.add(e);
-            }
-        }
-        listToReturn.addAll(hs2);
-        return listToReturn;
-    }
-
-    private static <E> List<E> removeDoubleEntriesInList(List<E> list) {
-        Set<E> hs = new HashSet<E>();
-        hs.addAll(list);
-        list.clear();
-        list.addAll(hs);
-        return list;
-    }
-
     private static Map <Integer, List<Integer>> getSubBoardRankLists(int numberOValuesInSublist, List<Integer> boardRanks) {
         int numberOfLists;
         if(boardRanks.size() > numberOValuesInSublist) {
@@ -899,37 +697,6 @@ public class BoardEvaluator {
             }
         }
         return subBoardRankLists;
-    }
-
-    private static <T, E> T getKeyByValue(Map<T, E> map, E value) {
-        for (Map.Entry<T, E> entry : map.entrySet()) {
-            if (Objects.equals(value, entry.getValue())) {
-                return entry.getKey();
-            }
-        }
-        return null;
-    }
-
-    private static <T, E> Set<T> getKeysByValue(Map<T, E> map, E value) {
-        Set<T> keys = new HashSet<T>();
-        for (Map.Entry<T, E> entry : map.entrySet()) {
-            if (Objects.equals(value, entry.getValue())) {
-                keys.add(entry.getKey());
-            }
-        }
-        return keys;
-    }
-
-    private static List<Card> convertIntegerBoardToArtificialCardBoard(List<Integer> integerBoarList) {
-        Map<Integer, Card> newCardObjects = new HashMap<Integer, Card>();
-        for(int i = 0; i < integerBoarList.size(); i++) {
-            newCardObjects.put(i, new Card(integerBoarList.get(i), 'd'));
-        }
-        List<Card> artificialCardBoard = new ArrayList<Card>();
-        for(int i = 0; i < integerBoarList.size(); i++) {
-            artificialCardBoard.add(newCardObjects.get(i));
-        }
-        return artificialCardBoard;
     }
 
     private static List<List<Integer>> getTwoCardsThatMakeStraight(List<Card> board, List<Integer> subBoardRanks, int number) {
@@ -1090,103 +857,5 @@ public class BoardEvaluator {
             }
         }
         return straightCombos;
-    }
-
-    private static boolean boardContainsAce(List<Card> board) {
-        List<Integer> boardRanks = getSortedCardRanksFromCardList(board);
-        Integer ace = 14;
-        if(boardRanks.contains(ace)) {
-            return true;
-        }
-        return false;
-    }
-
-    private static boolean comboContainsLowAce(List<Integer> combo) {
-        for(Integer i : combo) {
-            if(i == 1) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    private static List<Integer> addLowAceToSubBoardRanksAndRemoveHighAceIfPresent(List<Integer> subBoardRanks, int wantedSubBoardRanksSize) {
-        Integer aceLow = 1;
-        Integer aceHigh = 14;
-        subBoardRanks.add(aceLow);
-        if(subBoardRanks.contains(aceHigh)) {
-            subBoardRanks.remove(aceHigh);
-        }
-        Collections.sort(subBoardRanks);
-        if(subBoardRanks.size() == wantedSubBoardRanksSize + 1) {
-            subBoardRanks.remove(wantedSubBoardRanksSize);
-        }
-        return subBoardRanks;
-    }
-
-    private static List<Integer> convertComboWithLowAceToHighAce(List<Integer> combo) {
-        Integer aceLow = 1;
-        Integer aceHigh = 14;
-        combo.remove(aceLow);
-        combo.add(aceHigh);
-        Collections.sort(combo);
-        return combo;
-    }
-
-    private static List<Integer> makeCopyOfComboToAddToReturnList(List<Integer> combo) {
-        List<Integer> copiedCombo = new ArrayList<Integer>();
-        copiedCombo.addAll(combo);
-        return copiedCombo;
-    }
-
-
-    private static List<Integer> addSecondCardToCreateComboWhenSingleCardMakesStraight(List<Integer> combo, Integer i) {
-        List<Integer> createdCombo = new ArrayList<Integer>();
-        createdCombo.add(i);
-        createdCombo.addAll(combo);
-        Collections.sort(createdCombo);
-        return createdCombo;
-    }
-
-    private static int getValueOfHighestCardOnBoard (List<Card> board) {
-        List<Integer> boardRanks = getSortedCardRanksFromCardList(board);
-        return boardRanks.get(boardRanks.size() - 1);
-    }
-
-    public static List<BooleanResult> allFunctions(List<Card> board) {
-        BooleanResult result1 = new BooleanResult();
-        BooleanResult result2 = new BooleanResult();
-        BooleanResult result3 = new BooleanResult();
-        BooleanResult result4 = new BooleanResult();
-        BooleanResult result5 = new BooleanResult();
-        BooleanResult result6 = new BooleanResult();
-        BooleanResult result7 = new BooleanResult();
-
-        result1.setFunctionDescription("Is board rainbow");
-        result1.setResult(isBoardRainbow(board));
-        result2.setFunctionDescription("Has board two of one suit");
-        result2.setResult(hasBoardTwoOfOneSuit(board));
-        result3.setFunctionDescription("Is board suited");
-        result3.setResult(isBoardSuited(board));
-        result4.setFunctionDescription("Is board connected");
-        result4.setResult(isBoardConnected(board));
-        result5.setFunctionDescription("Has board two connecting cards");
-        result5.setResult(hasBoardTwoConnectingCards(board));
-        result6.setFunctionDescription("Is board paired once");
-        result6.setResult(isBoardPairedOnce(board));
-        result7.setFunctionDescription("is er wheel activity?");
-        result7.setResult(boardContainsAceAndWheelCard(board));
-
-        List<BooleanResult> listOfFunctionResults = new ArrayList<BooleanResult>();
-
-        listOfFunctionResults.add(result1);
-        listOfFunctionResults.add(result2);
-        listOfFunctionResults.add(result3);
-        listOfFunctionResults.add(result4);
-        listOfFunctionResults.add(result5);
-        listOfFunctionResults.add(result6);
-        listOfFunctionResults.add(result7);
-
-        return listOfFunctionResults;
     }
 }
