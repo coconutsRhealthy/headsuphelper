63109cc20f25238df23022acc38e584779fbbf3f
coconutsRhealthy <krillin80@hotmail.com>
2016-08-31 19:32:22 +0200

    Further developed PairEvaluator

--- src/main/java/com/lennart/model/boardevaluation/PairEvaluator.java
+++ src/main/java/com/lennart/model/boardevaluation/PairEvaluator.java
@@ -25,36 +25,64 @@ public class PairEvaluator extends BoardEvaluator {
 
         //als het board unpaired is, dan...
 
-        //je wil checken dat van jouw lijstje slechts een value overeenkomt met de boardkaart
-        for(Card c : board) {
-            for (Map.Entry<Integer, List<Integer>> entry : allPossibleStartHandsRankOnly.entrySet()) {
-                if(Collections.frequency(entry.getValue(), c.getRank()) == 1) {
-                    combosThatMakePair.put(combosThatMakePair.size(), allPossibleStartHands.get(entry.getKey()));
+        if(getNumberOfPairsOnBoard(board) == 0) {
+            //je wil checken dat van jouw lijstje slechts een value overeenkomt met de boardkaart
+            for(Card c : board) {
+                for (Map.Entry<Integer, List<Integer>> entry : allPossibleStartHandsRankOnly.entrySet()) {
+                    if(Collections.frequency(entry.getValue(), c.getRank()) == 1) {
+                        combosThatMakePair.put(combosThatMakePair.size(), allPossibleStartHands.get(entry.getKey()));
+                    }
                 }
             }
+
+            //voeg ook nog de pocket pairs toe
+            Map<Integer, List<Card>> allPocketPairs = getAllPocketPairStartHands();
+            //allPocketPairs = clearStartHandsMapOfStartHandsThatContainCardsOnTheBoard(allPocketPairs, board);
+
+            allPocketPairs = clearStartHandsMapOfStartHandsThatContainCardOfSpecificRank(allPocketPairs, board.get(0).getRank());
+            allPocketPairs = clearStartHandsMapOfStartHandsThatContainCardOfSpecificRank(allPocketPairs, board.get(1).getRank());
+            allPocketPairs = clearStartHandsMapOfStartHandsThatContainCardOfSpecificRank(allPocketPairs, board.get(2).getRank());
+
+            for (Map.Entry<Integer, List<Card>> entry : allPocketPairs.entrySet()) {
+                combosThatMakePair.put(combosThatMakePair.size(), entry.getValue());
+            }
+
+            return combosThatMakePair;
         }
-
-        //voeg ook nog de pocket pairs toe
-        Map<Integer, List<Card>> allPocketPairs = getAllPocketPairStartHands();
-        //allPocketPairs = clearStartHandsMapOfStartHandsThatContainCardsOnTheBoard(allPocketPairs, board);
-
-        allPocketPairs = clearStartHandsMapOfStartHandsThatContainCardOfSpecificRank(allPocketPairs, board.get(0).getRank());
-        allPocketPairs = clearStartHandsMapOfStartHandsThatContainCardOfSpecificRank(allPocketPairs, board.get(1).getRank());
-        allPocketPairs = clearStartHandsMapOfStartHandsThatContainCardOfSpecificRank(allPocketPairs, board.get(2).getRank());
-
-        for (Map.Entry<Integer, List<Card>> entry : allPocketPairs.entrySet()) {
-            combosThatMakePair.put(combosThatMakePair.size(), entry.getValue());
-        }
-
-        return combosThatMakePair;
 
         //als het board een pair heeft, dan...
+        //leveren alle combos die verder niet pairen met het board of trips maken, een pair..
+        if(getNumberOfPairsOnBoard(board) == 1) {
+            int counter = 0;
+            for (Map.Entry<Integer, List<Integer>> entry : allPossibleStartHandsRankOnly.entrySet()) {
+                for(Card c : board) {
+                    if (Collections.frequency(entry.getValue(), c.getRank()) == 0) {
+                        counter++;
+                    }
+                    if (counter == board.size()) {
+                        combosThatMakePair.put(combosThatMakePair.size(), allPossibleStartHands.get(entry.getKey()));
+                    }
+                }
+                counter = 0;
+            }
+
+            //remove nu nog alle pocketpairs
+            System.out.println("eije");
+            for(Iterator<Map.Entry<Integer, List<Card>>> it = combosThatMakePair.entrySet().iterator(); it.hasNext(); ) {
+                Map.Entry<Integer, List<Card>> entry = it.next();
+                if(entry.getValue().get(0).getRank() == entry.getValue().get(1).getRank()) {
+                    it.remove();
+                }
+            }
+            return combosThatMakePair;
+        }
 
         //als het board twee pair heeft, dan...
 
         //als het board trips heeft, dan...
 
         //etc
+        return null;
     }
 
     //helper methods
