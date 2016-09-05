4dcc8b1f324210dca399bce0876e3c2c3faa427d
Lennart Popma <lennart.popma@ordina.nl>
2016-09-05 16:01:15 +0200

    Implemented Comparable in Card class and created comboComparator method

--- src/main/java/com/lennart/model/pokergame/Card.java
+++ src/main/java/com/lennart/model/pokergame/Card.java
@@ -1,9 +1,13 @@
 package com.lennart.model.pokergame;
 
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
 /**
  * Created by Lennart Popma on 3-6-2016.
  */
-public class Card {
+public class Card implements Comparable<Card> {
     private int rank;
     private char suit;
 
@@ -48,5 +52,37 @@ public class Card {
         int result = rank;
         result = 31 * result + (int) suit;
         return result;
+    }
+
+    @Override
+    public int compareTo(Card c) {
+            if(c.getRank() > this.getRank()) {
+                return 1;
+            } else if(c.getRank() == this.getRank()) {
+                return 0;
+            } else {
+                return -1;
+            }
+    }
+
+    static Comparator<List<Card>> getComboComparator() {
+        return new Comparator<List<Card>>() {
+            @Override
+            public int compare(List<Card> combo1, List<Card> combo2) {
+                Collections.sort(combo1);
+                Collections.sort(combo2);
+
+                if(combo2.get(0).getRank() > combo1.get(0).getRank()) {
+                    return 1;
+                } else if(combo2.get(0).getRank() == combo1.get(0).getRank()) {
+                    if(combo2.get(1).getRank() > combo1.get(1).getRank()) {
+                        return 1;
+                    } else if(combo2.get(1).getRank() == combo1.get(1).getRank()) {
+                        return 0;
+                    }
+                }
+                return -1;
+            }
+        };
     }
 }
