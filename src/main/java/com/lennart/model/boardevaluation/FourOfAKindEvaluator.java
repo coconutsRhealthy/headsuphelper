a697e239188b833a13edad94b54e4a10bc3b88e1
Lennart Popma <lennart.popma@ordina.nl>
2016-09-23 17:27:01 +0200

    Created FourOfAKindEvaluator

---
+++ src/main/java/com/lennart/model/boardevaluation/FourOfAKindEvaluator.java
@@ -0,0 +1,31 @@
+package com.lennart.model.boardevaluation;
+
+import com.lennart.model.pokergame.Card;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * Created by lpo10346 on 9/23/2016.
+ */
+public class FourOfAKindEvaluator {
+
+    public Map<Integer, Set<Set<Card>>> getFourOfAKindCombos(List<Card> board) {
+        Map<Integer, Set<Set<Card>>> fourOfAKindCOmbos = new HashMap<>();
+
+        //als geen pair op het board
+        return fourOfAKindCOmbos;
+
+        //als een pair op het board
+
+        //als twee pair op het board
+
+        //als three of a kind op het board
+
+        //als four of a kind op het board
+
+
+    }
+}
