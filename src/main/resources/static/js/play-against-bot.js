142c6c0205dbfea3c8fd4337042abcd070b373a6
Lennart Popma <lennartpopma@hotmail.com>
2016-12-12 17:53:49 +0100

    Changes to render both holecards correctly

--- src/main/resources/static/js/play-against-bot.js
+++ src/main/resources/static/js/play-against-bot.js
@@ -6,27 +6,29 @@ mainApp.controller('pokerController', function($scope, $http) {
     $scope.holeCard2;
 
     $scope.holeCard1SuitWritten;
+    $scope.holeCard2SuitWritten;
 
     $scope.holeCard1Class;
     $scope.holeCard1SuitUniCode;
 
-    $scope.suitWritten;
-
-
+    $scope.holeCard2Class;
+    $scope.holeCard2SuitUniCode;
 
     $scope.startGame = function() {
         $http.get('/startGame/').success(function(data) {
-            $scope.holeCard1 = data.myHand[1];
-            $scope.holeCard2 = data.myHand[2];
+            $scope.holeCard1 = data.myHand[0];
+            $scope.holeCard2 = data.myHand[1];
 
-            setSuitWritten();
+            $scope.holeCard1.rank = convertRankFromIntegerToRank($scope.holeCard1.rank);
+            $scope.holeCard2.rank = convertRankFromIntegerToRank($scope.holeCard2.rank);
+
+            setHoleCard1SuitWritten();
+            setHoleCard2SuitWritten();
             setHoleCardsCss();
-
-            alert(JSON.stringify($scope.holeCard1));
         })
     }
 
-    function setSuitWritten() {
+    function setHoleCard1SuitWritten() {
         switch($scope.holeCard1.suit) {
             case 's':
                 $scope.holeCard1SuitWritten = "spades";
@@ -47,7 +49,51 @@ mainApp.controller('pokerController', function($scope, $http) {
         }
     }
 
+    function setHoleCard2SuitWritten() {
+        switch($scope.holeCard2.suit) {
+            case 's':
+                $scope.holeCard2SuitWritten = "spades";
+                $scope.holeCard2SuitUniCode = "\u2660";
+                break;
+            case 'c':
+                $scope.holeCard2SuitWritten = "clubs";
+                $scope.holeCard2SuitUniCode = "\u2663";
+                break;
+            case 'd':
+                $scope.holeCard2SuitWritten = "diams";
+                $scope.holeCard2SuitUniCode = "\u2666";
+                break;
+            case 'h':
+                $scope.holeCard2SuitWritten = "hearts";
+                $scope.holeCard2SuitUniCode = "\u2665";
+                break;
+        }
+    }
+
     function setHoleCardsCss() {
         $scope.holeCard1Class = "card rank-" + $scope.holeCard1.rank + " " + $scope.holeCard1SuitWritten;
+        $scope.holeCard2Class = "card rank-" + $scope.holeCard2.rank + " " + $scope.holeCard2SuitWritten;
+    }
+
+    function convertRankFromIntegerToRank(rankCard) {
+        switch(rankCard) {
+            case 14:
+                return 'A';
+                break;
+            case 13:
+                return 'K';
+                break;
+            case 12:
+                return 'Q';
+                break;
+            case 11:
+                return 'J';
+                break;
+            case 10:
+                return 'T';
+                break;
+            default:
+                return rankCard;
+        }
     }
 });
