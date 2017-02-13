e4c82aed391dde143d284160dad6d61efb52b35e
LennartMac <lennartpopma@hotmail.com>
2017-02-13 22:25:23 +0100

    Further development of ImageProcessor. WIP

--- src/main/java/com/lennart/model/imageprocessing/ImageProcessor.java
+++ src/main/java/com/lennart/model/imageprocessing/ImageProcessor.java
@@ -20,17 +20,17 @@ import static org.bytedeco.javacpp.lept.pixRead;
  */
 public class ImageProcessor {
 
-    public void createPartialSreenShot(int x, int y, int width, int height) {
+    public void createPartialSreenShot(int x, int y, int width, int height, String path) {
         Point point = new Point(x, y);
         Dimension dimension = new Dimension(width, height);
         Rectangle rectangle = new Rectangle(point, dimension);
 
         try {
             BufferedImage screenCapture = new Robot().createScreenCapture(rectangle);
-            ImageIO.write(screenCapture, "png", new File("D:/screenshot.png"));
-            //ImageIO.write(screenCapture, "png", new File("/Users/LennartMac/Desktop/screenshot.png"));
+            //ImageIO.write(screenCapture, "png", new File("D:/screenshot.png"));
+            ImageIO.write(screenCapture, "png", new File(path));
 
-            zoomInImage(screenCapture.getWidth(), screenCapture.getHeight(), 3, screenCapture);
+            //zoomInImage(screenCapture.getWidth(), screenCapture.getHeight(), 3, screenCapture);
         } catch (IOException | AWTException e) {
             System.out.println("Exception occured in createScreenShot: " + e.getMessage());
         }
@@ -73,20 +73,14 @@ public class ImageProcessor {
         return rgbList;
     }
 
-    public void zoomInImage(int imageWidth, int imageHeight, int zoomLevel, BufferedImage originalImage) {
-        int newImageWidth = imageWidth * zoomLevel;
-        int newImageHeight = imageHeight * zoomLevel;
+    public BufferedImage zoomInImage(BufferedImage originalImage, int zoomLevel) {
+        int newImageWidth = originalImage.getWidth() * zoomLevel;
+        int newImageHeight = originalImage.getHeight() * zoomLevel;
         BufferedImage resizedImage = new BufferedImage(newImageWidth , newImageHeight, 1);
         Graphics2D g = resizedImage.createGraphics();
         g.drawImage(originalImage, 0, 0, newImageWidth , newImageHeight , null);
         g.dispose();
-
-        try {
-            ImageIO.write(resizedImage, "png", new File("D:/zoomedScreenshot.png"));
-        } catch (Exception e) {
-
-        }
-
+        return resizedImage;
     }
 
     public String getStringFromImageWithTesseract(String pathOfImage) {
@@ -114,14 +108,57 @@ public class ImageProcessor {
         return string;
     }
 
-    public static void main(String[] args) {
+    private void saveBufferedImage(BufferedImage bufferedImage, String path) throws IOException {
+        ImageIO.write(bufferedImage, "png", new File(path));
+    }
+
+    public double getMouseXcoordinate() {
+        return MouseInfo.getPointerInfo().getLocation().getX();
+    }
+
+    public double getMouseYcoordinate() {
+        return MouseInfo.getPointerInfo().getLocation().getY();
+    }
+
+    private void createTopPlayerUserNameScreenShot() throws IOException {
+        int x = 486;
+        int y = 187;
+        int width = 115;
+        int height = 27;
+        String path = "/Users/LennartMac/Desktop/topPlayerUserName.png";
+
+        BufferedImage topPlayerUserName = getBufferedImageScreenShot(x, y, width, height);
+        saveBufferedImage(topPlayerUserName, path);
+    }
+
+    private void createTopPlayerStackScreenShot() throws IOException {
+        int x = 496;
+        int y = 223;
+        int width = 95;
+        int height = 25;
+        String path = "/Users/LennartMac/Desktop/topPlayerStack.png";
+
+        BufferedImage topPlayerUserName = getBufferedImageScreenShot(x, y, width, height);
+        saveBufferedImage(topPlayerUserName, path);
+    }
+
+    private void createPotSizeScreenShot() throws IOException{
+        int x = 440;
+        int y = 327;
+        int width = 130;
+        int height = 25;
+        String path = "/Users/LennartMac/Desktop/potSize.png";
+
+        BufferedImage potSizeScreenShot = getBufferedImageScreenShot(x, y, width, height);
+        potSizeScreenShot = zoomInImage(potSizeScreenShot, 2);
+        saveBufferedImage(potSizeScreenShot, path);
+    }
+
+    public static void main(String[] args) throws Exception {
         ImageProcessor imageProcessor = new ImageProcessor();
 
-        //imageProcessor.createPartialSreenShot(310, 300, 300, 63);
-        imageProcessor.createPartialSreenShot(0, 0, 100, 100);
-
-        //String string = imageProcessor.getStringFromImageWithTesseract("/Users/LennartMac/Desktop/screenshot.png");
-
-        //System.out.println(string);
+        imageProcessor.createPotSizeScreenShot();
+        String s = imageProcessor.getStringFromImageWithTesseract("/Users/LennartMac/Desktop/potSize.png");
+        System.out.println(s);
     }
 }
