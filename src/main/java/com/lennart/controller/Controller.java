2338afbeb3f2e7e4eef61821cc6501cb5835305d
Lennart Popma <lennartpopma@hotmail.com>
2018-01-31 17:35:41 +0100

    Added new methods to Controller for new fe db page

--- src/main/java/com/lennart/controller/Controller.java
+++ src/main/java/com/lennart/controller/Controller.java
@@ -1,12 +1,17 @@
 package com.lennart.controller;
 
+import com.lennart.model.action.actionbuilders.ai.Poker;
+import com.lennart.model.action.actionbuilders.ai.SimulatedHand;
 import com.lennart.model.botgame.BotTable;
-import com.lennart.model.computergame.ComputerGame;
 import com.lennart.model.computergame.ComputerGameNew;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
 
 
 @Configuration
@@ -57,6 +62,24 @@ public class Controller {
         new BotTable(true);
     }
 
+    @RequestMapping(value = "/fillDbs", method = RequestMethod.GET)
+    private void fillDbs() throws Exception {
+        Poker poker = new Poker();
+        List<String> routes = poker.getAllRoutes();
+        poker.storeRoutesInDb(routes);
+    }
+
+    @RequestMapping(value = "/startHandSimulation", method = RequestMethod.GET)
+    private void startHandSimulation() throws Exception {
+        for(int i = 0; i < 4_000_000; i++) {
+            Random rn = new Random();
+            int y = rn.nextInt(2 - 1 + 1) + 1;
+
+            SimulatedHand simulatedHand = new SimulatedHand(y);
+            Map<String, Double> scores = simulatedHand.playHand();
+            simulatedHand.updatePayoff(scores.get("aiBot"));
+        }
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Controller.class, args);
