6611459bd82bb1be4c5c2cf78a13c7e5ff904876
Lennart Popma <lennartpopma@hotmail.com>
2018-03-01 11:18:12 +0100

    Disable, for now, to perform action of the site

--- src/main/java/com/lennart/model/botgame/BotHand.java
+++ src/main/java/com/lennart/model/botgame/BotHand.java
@@ -150,7 +150,7 @@ public class BotHand {
         botAction = new Poker().getAction(this, eligibleActions, streetInMethod, botIsButtonInMethod, potSizeBb, opponentActionInMethod, facingOdds, effectiveStack, botHasStrongDrawInMethod, botHandStrengthInMethod, opponentType, opponentBetsizeBb, botBetsizeBb, opponentStackBb, botStackBb, preflop, boardInMethod);
         sizing = new Sizing().getAiBotSizing(opponentTotalBetSize, botTotalBetSize, botStack, opponentStack, potSize, bigBlind, board);
 
-        NetBetTableReader.performActionOnSite(botAction, sizing);
+        //NetBetTableReader.performActionOnSite(botAction, sizing);
     }
 
     private List<String> getEligibleActions() {
