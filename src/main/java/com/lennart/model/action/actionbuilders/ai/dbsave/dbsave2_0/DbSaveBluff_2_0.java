71d079a2b46583d2f97b54cd8ed08a581a5ce286
LennartMac <lennartpopma@hotmail.com>
2019-01-21 12:01:33 +0100

    Further development of DbSaveBluff_2_0 and OpponentIdentifier2_0

--- src/main/java/com/lennart/model/action/actionbuilders/ai/dbsave/dbsave2_0/DbSaveBluff_2_0.java
+++ src/main/java/com/lennart/model/action/actionbuilders/ai/dbsave/dbsave2_0/DbSaveBluff_2_0.java
@@ -1,5 +1,6 @@
 package com.lennart.model.action.actionbuilders.ai.dbsave.dbsave2_0;
 
+import com.lennart.model.action.actionbuilders.ai.opponenttypes.opponentidentifier_2_0.OpponentIdentifier2_0;
 import com.lennart.model.card.Card;
 
 import java.util.List;
@@ -15,8 +16,14 @@ public class DbSaveBluff_2_0 {
         String boardWetnessGroup = "";
 
         if(board.size() == 3) {
-            //flop, to do
-
+            //flushStraightWetness number
+            if(boardWetness <= 112) {
+                boardWetnessGroup = "dry";
+            } else if(boardWetness <= 167) {
+                boardWetnessGroup = "medium";
+            } else {
+                boardWetnessGroup = "wet";
+            }
         } else if(board.size() == 4) {
             if(boardWetness <= 67) {
                 boardWetnessGroup = "wet";
@@ -38,5 +45,97 @@ public class DbSaveBluff_2_0 {
         return boardWetnessGroup;
     }
 
+    public String getOpponentPreflopTypeLogig(String opponentName, boolean includingMedium) throws Exception {
+        String opponentType;
 
+        OpponentIdentifier2_0 opponentIdentifier2_0 = new OpponentIdentifier2_0();
+
+        List<Double> preflopData = opponentIdentifier2_0.getOpponentLoosenessAndAggroness(opponentName, true);
+
+        if(preflopData.isEmpty()) {
+            opponentType = "uu";
+        } else {
+            double preflopLooseness = preflopData.get(0);
+            double preflopAggroness = preflopData.get(1);
+
+            if(includingMedium) {
+                if(preflopLooseness < 0.67) {
+                    opponentType = "t";
+                } else if(preflopLooseness < 0.79) {
+                    opponentType = "m";
+                } else {
+                    opponentType = "l";
+                }
+
+                if(preflopAggroness < 0.32) {
+                    opponentType = opponentType + "p";
+                } else if(preflopAggroness < 0.45) {
+                    opponentType = opponentType + "m";
+                } else {
+                    opponentType = opponentType + "a";
+                }
+            } else {
+                if(preflopLooseness < 0.73) {
+                    opponentType = "t";
+                } else {
+                    opponentType = "l";
+                }
+
+                if(preflopAggroness < 0.38) {
+                    opponentType = opponentType + "p";
+                } else {
+                    opponentType = opponentType + "a";
+                }
+            }
+        }
+
+        return opponentType;
+    }
+
+    public String getOpponentPostflopTypeLogig(String opponentName, boolean includingMedium) throws Exception {
+        String opponentType;
+
+        OpponentIdentifier2_0 opponentIdentifier2_0 = new OpponentIdentifier2_0();
+
+        List<Double> postflopData = opponentIdentifier2_0.getOpponentLoosenessAndAggroness(opponentName, false);
+
+        if(postflopData.isEmpty()) {
+            opponentType = "uu";
+        } else {
+            double postflopLoosenes = postflopData.get(0);
+            double postflopAggroness = postflopData.get(1);
+
+            if(includingMedium) {
+                if(postflopLoosenes < 0.46) {
+                    opponentType = "t";
+                } else if(postflopLoosenes < 0.54) {
+                    opponentType = "m";
+                } else {
+                    opponentType = "l";
+                }
+
+                if(postflopAggroness < 0.30) {
+                    opponentType = opponentType + "p";
+                } else if(postflopAggroness < 0.40) {
+                    opponentType = opponentType + "m";
+                } else {
+                    opponentType = opponentType + "a";
+                }
+            } else {
+                if(postflopLoosenes < 0.5) {
+                    opponentType = "t";
+                } else {
+                    opponentType = "l";
+                }
+
+                if(postflopAggroness < 0.33) {
+                    opponentType = opponentType + "p";
+                } else {
+                    opponentType = opponentType + "a";
+                }
+            }
+        }
+
+        return opponentType;
+    }
 }
--- src/main/java/com/lennart/model/action/actionbuilders/ai/opponenttypes/opponentidentifier_2_0/OpponentIdentifier2_0.java
+++ src/main/java/com/lennart/model/action/actionbuilders/ai/opponenttypes/opponentidentifier_2_0/OpponentIdentifier2_0.java
@@ -1,9 +1,10 @@
 package com.lennart.model.action.actionbuilders.ai.opponenttypes.opponentidentifier_2_0;
 
 import com.lennart.model.action.actionbuilders.ai.HandHistoryReaderStars;
+import com.lennart.model.action.actionbuilders.ai.dbsave.dbsave2_0.DbSaveBluff_2_0;
 
 import java.sql.*;
-import java.util.List;
+import java.util.*;
 
 /**
  * Created by LennartMac on 12/01/2019.
@@ -12,8 +13,134 @@ public class OpponentIdentifier2_0 {
 
     private Connection con;
 
-    public static void main(String[] args) throws Exception {
-        new OpponentIdentifier2_0().updateOpponentIdentifier2_0_db("sjaakie", 20);
+    private void testMethod() throws Exception {
+        initializeDbConnection();
+
+        Statement st = con.createStatement();
+        ResultSet rs = st.executeQuery("SELECT * FROM opponentidentifier_2_0_preflop;");
+
+        while(rs.next()) {
+            String opponentName = rs.getString("playerName");
+
+            DbSaveBluff_2_0 dbSaveBluff_2_0 = new DbSaveBluff_2_0();
+
+            String preflopType = dbSaveBluff_2_0.getOpponentPreflopTypeLogig(opponentName, true);
+            String postflopType = dbSaveBluff_2_0.getOpponentPostflopTypeLogig(opponentName, true);
+
+            System.out.println(preflopType + postflopType);
+        }
+
+        rs.close();
+        st.close();
+
+        closeDbConnection();
+    }
+
+    private void printLoosenessAndTightnessBoundries(boolean includingMedium, String table) throws Exception {
+        List<Double> allLooseness = new ArrayList<>();
+        List<Double> allAggro = new ArrayList<>();
+
+        initializeDbConnection();
+
+        Statement st = con.createStatement();
+        ResultSet rs = st.executeQuery("SELECT * FROM " + table + ";");
+
+        while(rs.next()) {
+            if(rs.getDouble("numberOfHands") >= 20) {
+                double callTotal = rs.getDouble("callCount");
+                double foldTotal = rs.getDouble("foldCount");
+                double betTotal;
+
+                if(table.contains("postflop")) {
+                    betTotal = rs.getDouble("betCount");
+                } else {
+                    betTotal = 0;
+                }
+
+                double raiseTotal = rs.getDouble("raiseCount");
+                double checkTotal = rs.getDouble("checkCount");
+
+                double looseness = callTotal / (callTotal + foldTotal);
+                double aggressiveness;
+
+                if(table.contains("postflop")) {
+                    aggressiveness = (betTotal + raiseTotal) / (betTotal + raiseTotal + checkTotal + callTotal);
+                } else {
+                    aggressiveness = (raiseTotal) / (raiseTotal + checkTotal + callTotal);
+                }
+
+                allLooseness.add(looseness);
+                allAggro.add(aggressiveness);
+            }
+        }
+
+        Collections.sort(allLooseness);
+        Collections.sort(allAggro);
+
+        if(!includingMedium) {
+            int halfLooseness = allLooseness.size() / 2;
+            int halfAggroness = allLooseness.size() / 2;
+
+            System.out.println("Looseness tight below: " + allLooseness.get(halfLooseness));
+            System.out.println("Aggro passive below: " + allAggro.get(halfAggroness));
+        } else {
+            int oneThirdLooseness = allLooseness.size() / 3;
+            int twoThirdLooseness = (allLooseness.size() / 3) * 2;
+
+            int oneThirdAggroness = allAggro.size() / 3;
+            int twoThirdAggroness = (allAggro.size() / 3) * 2;
+
+            System.out.println("Looseness tight below: " + allLooseness.get(oneThirdLooseness));
+            System.out.println("Looseness medium below: " + allLooseness.get(twoThirdLooseness));
+
+            System.out.println("Aggro passive below: " + allAggro.get(oneThirdAggroness));
+            System.out.println("Aggro medium below: " + allAggro.get(twoThirdAggroness));
+        }
+
+    }
+
+    public List<Double> getOpponentLoosenessAndAggroness(String opponentName, boolean preflop) throws Exception {
+        List<Double> oppLoosenessAndAggroness = new ArrayList<>();
+
+        String table;
+
+        if(preflop) {
+            table = "opponentidentifier_2_0_preflop";
+        } else {
+            table = "opponentidentifier_2_0_postflop";
+        }
+
+        initializeDbConnection();
+
+        Statement st = con.createStatement();
+        ResultSet rs = st.executeQuery("SELECT * FROM " + table + " WHERE playerName = '" + opponentName + "'");
+
+        if(rs.next()) {
+            if(rs.getDouble("numberOfHands") >= 20) {
+                double callTotal = rs.getDouble("callCount");
+                double foldTotal = rs.getDouble("foldCount");
+                double raiseTotal = rs.getDouble("raiseCount");
+                double checkTotal = rs.getDouble("checkCount");
+
+                double looseness;
+                double aggressiveness;
+
+                if(preflop) {
+                    looseness = callTotal / (callTotal + foldTotal);
+                    aggressiveness = raiseTotal / (raiseTotal + checkTotal + callTotal);
+                } else {
+                    double betTotal = rs.getDouble("betCount");
+
+                    looseness = callTotal / (callTotal + foldTotal);
+                    aggressiveness = (raiseTotal + betTotal) / (raiseTotal + betTotal + checkTotal + callTotal);
+                }
+
+                oppLoosenessAndAggroness.add(looseness);
+                oppLoosenessAndAggroness.add(aggressiveness);
+            }
+        }
+
+        return oppLoosenessAndAggroness;
     }
 
     public void updateOpponentIdentifier2_0_db(String opponentPlayerNameOfLastHand, double bigBlind) throws Exception {
