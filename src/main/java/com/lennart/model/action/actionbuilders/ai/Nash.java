305ab2b7166f787fda4bce9fc0507b1c5927cf6d
lennartpopma <lennart.popma@ordina.nl>
2019-03-22 18:48:44 +0100

    Initial version of Nash

---
+++ src/main/java/com/lennart/model/action/actionbuilders/ai/Nash.java
@@ -0,0 +1,297 @@
+package com.lennart.model.action.actionbuilders.ai;
+
+import com.lennart.model.card.Card;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Nash {
+
+    //postflop:
+        //pushen met 73% plus
+        //checkcallen met 56% plus
+
+
+
+    //DbSave#getComboLogic()
+
+    public double getMinEffStackToShove(List<Card> holeCards) {
+        return 0;
+    }
+
+    public double getMinEffStackToCall(List<Card> holeCards) {
+        return 0;
+    }
+
+    private void aMethod() {
+        //push
+
+        List<String> _20_group = new ArrayList<>();
+        List<String> _19_5_group = new ArrayList<>();
+        List<String> _18_3_group = new ArrayList<>();
+        List<String> _18_group = new ArrayList<>();
+        List<String> _17_6_group = new ArrayList<>();
+        List<String> _17_1_group = new ArrayList<>();
+        List<String> _16_6_group = new ArrayList<>();
+        List<String> _16_1_group = new ArrayList<>();
+        List<String> _15_8_group = new ArrayList<>();
+        List<String> _15_3_group = new ArrayList<>();
+        List<String> _15_2_group = new ArrayList<>();
+        List<String> _15_group = new ArrayList<>();
+        List<String> _14_3_group = new ArrayList<>();
+        List<String> _13_8_group = new ArrayList<>();
+        List<String> _13_4_group = new ArrayList<>();
+        List<String> _13_2_group = new ArrayList<>();
+        List<String> _13_group = new ArrayList<>();
+        List<String> _12_7_group = new ArrayList<>();
+        List<String> _12_4_group = new ArrayList<>();
+        List<String> _12_1_group = new ArrayList<>();
+        List<String> _11_7_group = new ArrayList<>();
+        List<String> _11_5_group = new ArrayList<>();
+        List<String> _11_4_group = new ArrayList<>();
+        List<String> _11_group = new ArrayList<>();
+        List<String> _10_7_group = new ArrayList<>();
+        List<String> _11_6_group = new ArrayList<>();
+        List<String> _10_5_group = new ArrayList<>();
+        List<String> _10_2_group = new ArrayList<>();
+        List<String> _9_9_group = new ArrayList<>();
+        List<String> _9_7_group = new ArrayList<>();
+        List<String> _9_5_group = new ArrayList<>();
+        List<String> _9_3_group = new ArrayList<>();
+        List<String> _9_1_group = new ArrayList<>();
+        List<String> _8_9_group = new ArrayList<>();
+        List<String> _8_8_group = new ArrayList<>();
+        List<String> _8_7_group = new ArrayList<>();
+        List<String> _8_4_group = new ArrayList<>();
+        List<String> _8_2_group = new ArrayList<>();
+        List<String> _8_1_group = new ArrayList<>();
+        List<String> _8_group = new ArrayList<>();
+        List<String> _7_8_group = new ArrayList<>();
+        List<String> _7_6_group = new ArrayList<>();
+        List<String> _7_4_group = new ArrayList<>();
+        List<String> _7_3_group = new ArrayList<>();
+        List<String> _7_2_group = new ArrayList<>();
+        List<String> _7_group = new ArrayList<>();
+        List<String> _6_9_group = new ArrayList<>();
+        List<String> _6_8_group = new ArrayList<>();
+        List<String> _6_6_group = new ArrayList<>();
+        List<String> _6_5_group = new ArrayList<>();
+        List<String> _6_4_group = new ArrayList<>();
+        List<String> _6_3_group = new ArrayList<>();
+        List<String> _6_2_group = new ArrayList<>();
+        List<String> _6_1_group = new ArrayList<>();
+        List<String> _6_group = new ArrayList<>();
+        List<String> _5_9_group = new ArrayList<>();
+        List<String> _5_8_group = new ArrayList<>();
+        List<String> _5_6_group = new ArrayList<>();
+        List<String> _5_5_group = new ArrayList<>();
+        List<String> _5_4_group = new ArrayList<>();
+        List<String> _5_2_group = new ArrayList<>();
+        List<String> _5_1_group = new ArrayList<>();
+        List<String> _5_group = new ArrayList<>();
+        List<String> _4_9_group = new ArrayList<>();
+        List<String> _4_8_group = new ArrayList<>();
+        List<String> _4_7_group = new ArrayList<>();
+        List<String> _4_6_group = new ArrayList<>();
+        List<String> _4_5_group = new ArrayList<>();
+        List<String> _4_3_group = new ArrayList<>();
+        List<String> _4_2_group = new ArrayList<>();
+        List<String> _4_1_group = new ArrayList<>();
+        List<String> _4_group = new ArrayList<>();
+        List<String> _3_9_group = new ArrayList<>();
+        List<String> _3_8_group = new ArrayList<>();
+        List<String> _3_7_group = new ArrayList<>();
+        List<String> _3_6_group = new ArrayList<>();
+        List<String> _3_5_group = new ArrayList<>();
+        List<String> _3_4_group = new ArrayList<>();
+        List<String> _3_3_group = new ArrayList<>();
+        List<String> _3_2_group = new ArrayList<>();
+        List<String> _3_1_group = new ArrayList<>();
+        List<String> _3_group = new ArrayList<>();
+        List<String> _2_9_group = new ArrayList<>();
+        List<String> _2_8_group = new ArrayList<>();
+        List<String> _2_7_group = new ArrayList<>();
+        List<String> _2_6_group = new ArrayList<>();
+
+//        List<String> _20_group = new ArrayList<>();
+        _20_group.add("AA");
+        _20_group.add("AKs");
+        _20_group.add("AQs");
+        _20_group.add("AJs");
+        _20_group.add("ATs");
+        _20_group.add("A9s");
+        _20_group.add("A8s");
+        _20_group.add("A7s");
+        _20_group.add("A6s");
+        _20_group.add("A5s");
+        _20_group.add("A4s");
+        _20_group.add("A3s");
+        _20_group.add("A2s");
+        _20_group.add("AKo");
+        _20_group.add("KK");
+        _20_group.add("KQs");
+        _20_group.add("KJs");
+        _20_group.add("KTs");
+        _20_group.add("K9s");
+        _20_group.add("K8s");
+        _20_group.add("K7s");
+        _20_group.add("K6s");
+        _20_group.add("K5s");
+        _20_group.add("K4s");
+        _20_group.add("AQo");
+        _20_group.add("KQo");
+        _20_group.add("QQ");
+        _20_group.add("QJs");
+        _20_group.add("QTs");
+        _20_group.add("Q9s");
+        _20_group.add("Q8s");
+        _20_group.add("Q7s");
+        _20_group.add("Q6s");
+        _20_group.add("Q5s");
+        _20_group.add("AJo");
+        _20_group.add("KJo");
+        _20_group.add("QJo");
+        _20_group.add("JJ");
+        _20_group.add("JTs");
+        _20_group.add("J9s");
+        _20_group.add("J8s");
+        _20_group.add("J7s");
+        _20_group.add("ATo");
+        _20_group.add("KTo");
+        _20_group.add("QTo");
+        _20_group.add("JTo");
+        _20_group.add("TT");
+        _20_group.add("T9s");
+        _20_group.add("T8s");
+        _20_group.add("T7s");
+        _20_group.add("T6s");
+        _20_group.add("A9o");
+        _20_group.add("K9o");
+        _20_group.add("Q9o");
+        _20_group.add("J9o");
+        _20_group.add("T9o");
+        _20_group.add("99");
+        _20_group.add("98s");
+        _20_group.add("97s");
+        _20_group.add("96s");
+        _20_group.add("A8o");
+        _20_group.add("98o");
+        _20_group.add("88");
+        _20_group.add("87s");
+        _20_group.add("86s");
+        _20_group.add("A7o");
+        _20_group.add("77");
+        _20_group.add("76s");
+        _20_group.add("75s");
+        _20_group.add("A6o");
+        _20_group.add("66");
+        _20_group.add("65s");
+        _20_group.add("A5o");
+        _20_group.add("55");
+        _20_group.add("54s");
+        _20_group.add("A4o");
+        _20_group.add("44");
+        _20_group.add("A3o");
+        _20_group.add("33");
+        _20_group.add("A2o");
+        _20_group.add("22");
+
+
+
+
+
+//        List<String> _19_5_group = new ArrayList<>();
+//        List<String> _18_3_group = new ArrayList<>();
+//        List<String> _18_group = new ArrayList<>();
+//        List<String> _17_6_group = new ArrayList<>();
+//        List<String> _17_1_group = new ArrayList<>();
+//        List<String> _16_6_group = new ArrayList<>();
+//        List<String> _16_1_group = new ArrayList<>();
+//        List<String> _15_8_group = new ArrayList<>();
+//        List<String> _15_3_group = new ArrayList<>();
+//        List<String> _15_2_group = new ArrayList<>();
+//        List<String> _15_group = new ArrayList<>();
+//        List<String> _14_3_group = new ArrayList<>();
+//        List<String> _13_8_group = new ArrayList<>();
+//        List<String> _13_4_group = new ArrayList<>();
+//        List<String> _13_2_group = new ArrayList<>();
+//        List<String> _13_group = new ArrayList<>();
+//        List<String> _12_7_group = new ArrayList<>();
+//        List<String> _12_4_group = new ArrayList<>();
+//        List<String> _12_1_group = new ArrayList<>();
+//        List<String> _11_7_group = new ArrayList<>();
+//        List<String> _11_5_group = new ArrayList<>();
+//        List<String> _11_4_group = new ArrayList<>();
+//        List<String> _11_group = new ArrayList<>();
+//        List<String> _10_7_group = new ArrayList<>();
+//        List<String> _11_6_group = new ArrayList<>();
+//        List<String> _10_5_group = new ArrayList<>();
+//        List<String> _10_2_group = new ArrayList<>();
+//        List<String> _9_9_group = new ArrayList<>();
+//        List<String> _9_7_group = new ArrayList<>();
+//        List<String> _9_5_group = new ArrayList<>();
+//        List<String> _9_3_group = new ArrayList<>();
+//        List<String> _9_1_group = new ArrayList<>();
+//        List<String> _8_9_group = new ArrayList<>();
+//        List<String> _8_8_group = new ArrayList<>();
+//        List<String> _8_7_group = new ArrayList<>();
+//        List<String> _8_4_group = new ArrayList<>();
+//        List<String> _8_2_group = new ArrayList<>();
+//        List<String> _8_1_group = new ArrayList<>();
+//        List<String> _8_group = new ArrayList<>();
+//        List<String> _7_8_group = new ArrayList<>();
+//        List<String> _7_6_group = new ArrayList<>();
+//        List<String> _7_4_group = new ArrayList<>();
+//        List<String> _7_3_group = new ArrayList<>();
+//        List<String> _7_2_group = new ArrayList<>();
+//        List<String> _7_group = new ArrayList<>();
+//        List<String> _6_9_group = new ArrayList<>();
+//        List<String> _6_8_group = new ArrayList<>();
+//        List<String> _6_6_group = new ArrayList<>();
+//        List<String> _6_5_group = new ArrayList<>();
+//        List<String> _6_4_group = new ArrayList<>();
+//        List<String> _6_3_group = new ArrayList<>();
+//        List<String> _6_2_group = new ArrayList<>();
+//        List<String> _6_1_group = new ArrayList<>();
+//        List<String> _6_group = new ArrayList<>();
+//        List<String> _5_9_group = new ArrayList<>();
+//        List<String> _5_8_group = new ArrayList<>();
+//        List<String> _5_6_group = new ArrayList<>();
+//        List<String> _5_5_group = new ArrayList<>();
+//        List<String> _5_4_group = new ArrayList<>();
+//        List<String> _5_2_group = new ArrayList<>();
+//        List<String> _5_1_group = new ArrayList<>();
+//        List<String> _5_group = new ArrayList<>();
+//        List<String> _4_9_group = new ArrayList<>();
+//        List<String> _4_8_group = new ArrayList<>();
+//        List<String> _4_7_group = new ArrayList<>();
+//        List<String> _4_6_group = new ArrayList<>();
+//        List<String> _4_5_group = new ArrayList<>();
+//        List<String> _4_3_group = new ArrayList<>();
+//        List<String> _4_2_group = new ArrayList<>();
+//        List<String> _4_1_group = new ArrayList<>();
+//        List<String> _4_group = new ArrayList<>();
+//        List<String> _3_9_group = new ArrayList<>();
+//        List<String> _3_8_group = new ArrayList<>();
+//        List<String> _3_7_group = new ArrayList<>();
+//        List<String> _3_6_group = new ArrayList<>();
+//        List<String> _3_5_group = new ArrayList<>();
+//        List<String> _3_4_group = new ArrayList<>();
+//        List<String> _3_3_group = new ArrayList<>();
+//        List<String> _3_2_group = new ArrayList<>();
+//        List<String> _3_1_group = new ArrayList<>();
+//        List<String> _3_group = new ArrayList<>();
+//        List<String> _2_9_group = new ArrayList<>();
+//        List<String> _2_8_group = new ArrayList<>();
+//        List<String> _2_7_group = new ArrayList<>();
+//        List<String> _2_6_group = new ArrayList<>();
+
+
+    }
+
+
+
+
+
+
+}
