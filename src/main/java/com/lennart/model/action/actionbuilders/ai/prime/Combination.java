758747e0e63ecc6e63c7fbf1e634870790bf74b7
LennartMac <lennartpopma@hotmail.com>
2019-10-18 11:09:11 +0200

    Created Combination algo class

---
+++ src/main/java/com/lennart/model/action/actionbuilders/ai/prime/Combination.java
@@ -0,0 +1,55 @@
+package com.lennart.model.action.actionbuilders.ai.prime;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Created by LennartMac on 14/10/2019.
+ */
+public class Combination {
+
+    private List<String> getAllCombinationsOfList(List<String> initialList) {
+        List<String> comboList = new ArrayList<>();
+
+        String[] arr = initialList.toArray(new String[] {});
+
+        for(int i = 0; i <= arr.length; i++) {
+            comboList.addAll(combinations(arr, i, 0, new String[i], comboList));
+        }
+
+        comboList = sanitizeComboList(comboList);
+
+        return comboList;
+    }
+
+    private List<String> combinations(String[] arr, int len, int startPosition, String[] result, List<String> combiList) {
+        if (len == 0){
+            combiList.add(Arrays.toString(result));
+            return combiList;
+        }
+        for (int i = startPosition; i <= arr.length-len; i++){
+            result[result.length - len] = arr[i];
+            combinations(arr, len-1, i+1, result, combiList);
+        }
+        return new ArrayList<>();
+    }
+
+    private List<String> sanitizeComboList(List<String> rawComboList) {
+        List<String> cleanComboList = new ArrayList<>();
+
+        rawComboList.removeAll(Collections.singleton("[]"));
+
+        for (String s : rawComboList) {
+            String toUse = s.replace("[", "");
+            toUse = toUse.replace("]", "");
+            toUse = toUse.replace(",", "");
+            toUse = toUse.replace(" ", "");
+
+            cleanComboList.add(toUse);
+        }
+
+        return cleanComboList;
+    }
+}
