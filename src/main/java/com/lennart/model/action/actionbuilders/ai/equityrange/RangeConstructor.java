9b97e1bbc80ff0b9fb0382cef2862788272f59c5
LennartMac <lennartpopma@hotmail.com>
2020-04-27 21:34:53 +0200

    RangeConstructor setup of all ranges dirty big commit

--- src/main/java/com/lennart/model/action/actionbuilders/ai/equityrange/RangeConstructor.java
+++ src/main/java/com/lennart/model/action/actionbuilders/ai/equityrange/RangeConstructor.java
@@ -1,4 +1,4 @@
-package com.lennart.model.action.actionbuilders.ai;
+package com.lennart.model.action.actionbuilders.ai.equityrange;
 
 import com.lennart.model.action.actionbuilders.ActionBuilderUtil;
 import com.lennart.model.boardevaluation.BoardEvaluator;
@@ -15,17 +15,27 @@ import java.util.stream.Collectors;
  */
 public class RangeConstructor {
 
-    public static void main(String[] args) {
-        new RangeConstructor().testMethod();
+
+
+    private void testMethodSingleCombo() {
+        List<Card> botHoleCards = Arrays.asList(new Card(14, 'c'), new Card(14, 'd'));
+        List<Card> board = Arrays.asList(new Card(2, 'd'), new Card(2, 'h'), new Card(2, 's'));
+
+        EquityCalculator equityCalculator = new EquityCalculator();
+
+        System.out.println(equityCalculator.getComboEquityFlop(botHoleCards, board));
     }
 
-    private void testMethod() {
+    private void testMethodRange() {
         List<Card> botHoleCards = Arrays.asList(new Card(3, 'c'), new Card(10, 'd'));
-        List<Card> board = Arrays.asList(new Card(2, 'd'), new Card(2, 'h'), new Card(2, 's'));
+        List<Card> board = Arrays.asList(new Card(7, 'h'), new Card(8, 'h'), new Card(9, 'h'));
 
         BoardEvaluator boardEvaluator = new BoardEvaluator(board);
 
-        List<List<Card>> oppRange = getOpponentRange(null, boardEvaluator, botHoleCards);
+        //List<List<Card>> oppRange = getOpponentRange(null, boardEvaluator, botHoleCards);
+        List<List<Card>> oppRange = ActionBuilderUtil.getAllPossibleStartHandsAsList().values().stream().collect(Collectors.toList());
+
+        oppRange = removeCombosWithKnownCards(oppRange, board);
 
         double averageEquity = getAverageEquityOfOppRange(oppRange, board);
         System.out.println(averageEquity);
@@ -34,11 +44,11 @@ public class RangeConstructor {
     private double getAverageEquityOfOppRange(List<List<Card>> oppRange, List<Card> board) {
         EquityCalculator equityCalculator = new EquityCalculator();
 
-        List<Double> equities = equityCalculator.getAllEquities(oppRange, board);
+        Map<List<Card>, Double> equities = equityCalculator.getRangeEquityFlop(oppRange, board);
 
         double average;
         try {
-            average = equities.stream()
+            average = equities.values().stream()
                     .mapToDouble(a -> a)
                     .average()
                     .getAsDouble();
@@ -48,6 +58,36 @@ public class RangeConstructor {
         }
 
         return average;
+    }
+
+    public static void main(String[] args) {
+//        List<List<Card>> allCombos = new PreflopEuityHs().getAllPreflopCombosEquitySorted();
+//        List<List<Card>> range = new RangeConstructor().getOppPre3betRange(allCombos, "high");
+//        System.out.println("wacht");
+//
+//        //System.out.println(new RangeConstructor().comboIsSuitedConnector(Arrays.asList(new Card(2, 'd'), new Card(14, 'd')), 1));
+
+        //new EquityCalculator().getComboEquityPreflop(Arrays.asList(new Card(8, 'd'), new Card(6, 'c')));
+
+        new RangeConstructor().ffEenTestAllEquitiesFlop();
+
+        //new EquityCalculator().getRangeEquityFlop()
+
+        //new RangeConstructor().testMethodRange();
+    }
+
+    private void ffEenTestAllEquitiesFlop() {
+        List<Card> board = Arrays.asList(new Card(8, 'c'), new Card(6, 's'), new Card(14, 'c'));
+        Map<List<Card>, Double> eije = getAllCombosEquitySortedFlop(board);
+        eije = sortByValueHighToLow(eije);
+        System.out.println("tjo");
+    }
+
+
+    private Map<List<Card>, Double> getAllCombosEquitySortedFlop(List<Card> board) {
+        List<List<Card>> allCombos = ActionBuilderUtil.getAllPossibleStartHandsAsList().values().stream().collect(Collectors.toList());
+        allCombos = removeCombosWithKnownCards(allCombos, board);
+        return new EquityCalculator().getRangeEquityFlop(allCombos, board);
     }
 
     private List<List<Card>> getOpponentRange(List<List<Card>> startingOppRange, BoardEvaluator boardEvaluator, List<Card> botHoleCards) {
@@ -85,6 +125,10 @@ public class RangeConstructor {
         opponentRange.addAll(rangeAsSet);
 
         return opponentRange;
+    }
+
+    private List<List<Card>> removeCombosWithKnownCards(List<List<Card>> range, List<Card> knownCards) {
+        return range.stream().filter(combo -> Collections.disjoint(combo, knownCards)).collect(Collectors.toList());
     }
 
     private List<List<Card>> createStartingOppRange(List<Card> botHoleCards) {
@@ -182,55 +226,303 @@ public class RangeConstructor {
 
 
 
+    //////
+
+    //more test for ranges
+
+
+
+    private void printStarthandsInOrder() {
+        Map<Integer, List<Card>> allStarthands = ActionBuilderUtil.getAllPossibleStartHandsAsList();
+
+        //Map<List<Card>, Double> eije = allStarthands.values().stream().map(combo -> new EquityCalculator().getComboEquityPreflop(combo)).collect(Collectors.toMap())
+
+        Map<List<Card>, Double> eije = allStarthands.values().stream().collect(Collectors.toMap(combo -> combo, combo -> new EquityCalculator().getComboEquityPreflop(combo)));
+
+
+        eije = sortByValueHighToLow(eije);
+
+        //eije.entrySet().stream().sorted().co
+
+        //eije = eije.entrySet().stream().sorted().collect(Collectors.toMap(a -> a, b -> b));
+
+        //List<Card> preflopComboToTest = Arrays.asList(new Card(9, 'd'), new Card(14, 'c'));
+
+        //System.out.println(new EquityCalculator().getComboEquityPreflop(preflopComboToTest));
+
+        System.out.println("wacht");
+
+    }
+
+    private <K, V extends Comparable<? super V>> Map<K, V> sortByValueHighToLow(Map<K, V> map) {
+        List<Map.Entry<K, V>> list = new LinkedList<>( map.entrySet() );
+        Collections.sort(list, new Comparator<Map.Entry<K, V>>() {
+            @Override
+            public int compare(Map.Entry<K, V> o1, Map.Entry<K, V> o2) {
+                return (o2.getValue() ).compareTo( o1.getValue());
+            }
+        });
+
+        Map<K, V> result = new LinkedHashMap<>();
+        for (Map.Entry<K, V> entry : list) {
+            result.put(entry.getKey(), entry.getValue());
+        }
+        return result;
+    }
 
 
 
 
-    private void getOppBetRangeYoOld() {
+
+    /////
+
+    private void getOppPre2betRange(List<List<Card>> sortedPreflopNashCombos) {
+        //input:
+            //pre2betGroup
+                //low
+                    //57% plus combo
+
+                //medium
+                    //25% plus combo
+                    //any suited
+
+                //high
+                    //any combo
+    }
+
+    private void getOppPreCall2betRange() {
+        //input:
+            //preCall2betGroup
+                //low
+                    //60% plus combo
+                    //all suited connectors and suited onegappers
+                //medium
+                    //40% plus combo
+                    //any suited
+                //high
+                    //any combo
+    }
+
+    private List<List<Card>> getOppPre3betRange(List<List<Card>> allSortedPfEquityCombos, String pre3betGroup) {
+        Set<List<Card>> oppPre3betRange = new LinkedHashSet<>();
+
+        double limit;
+
+        if(pre3betGroup.equals("mediumUnknown") || pre3betGroup.equals("medium")) {
+            limit = 0.30;
+        } else if(pre3betGroup.equals("low")) {
+            limit = 0.16;
+        } else if(pre3betGroup.equals("high")) {
+            limit = 0.37;
+        } else {
+            limit = 1;
+            System.out.println("pre3betGroup is unknown, should not come here");
+        }
+
+        for(int i = 0; i < allSortedPfEquityCombos.size() * limit; i++) {
+            oppPre3betRange.add(allSortedPfEquityCombos.get(i));
+        }
+
+        if(pre3betGroup.equals("mediumUnknown") || pre3betGroup.equals("medium")) {
+            List<List<Card>> extraCombos = allSortedPfEquityCombos.stream().filter(combo -> {
+                boolean suitedConnector = comboIsSuitedConnector(combo, 1) || comboIsSuitedConnector(combo, 2)
+                        || comboIsSuitedConnector(combo, 3);
+
+                return suitedConnector && combo.get(0).getRank() >= 4 && combo.get(1).getRank() >= 4;
+            }).collect(Collectors.toList());
+
+            oppPre3betRange.addAll(extraCombos);
+        }
+
+        if(pre3betGroup.equals("high")) {
+            List<List<Card>> extraCombos = allSortedPfEquityCombos.stream()
+                    .filter(combo -> {
+                        boolean suitedConnector = comboIsSuitedConnector(combo, 1) || comboIsSuitedConnector(combo, 2)
+                                || comboIsSuitedConnector(combo, 3);
+
+                        boolean suitedHigh = false;
+
+                        if(!suitedConnector) {
+                            if(combo.get(0).getSuit() == combo.get(1).getSuit()) {
+                                if(combo.get(0).getRank() >= 12 || combo.get(1).getRank() >= 12) {
+                                    suitedHigh = true;
+                                }
+                            }
+                        }
+
+                        return suitedConnector || suitedHigh;
+                    }).collect(Collectors.toList());
+
+            oppPre3betRange.addAll(extraCombos);
+        }
+
+        return oppPre3betRange.stream().collect(Collectors.toList());
+
 
         //input:
+            //pre3betGroup
+                //low
+                    //84% plus
+                //medium
+                    //70% plus
+                    //all suited connectors and suited onegappers and two-gappers
+                //high
+                    //50% plus
+                    //any suited
 
-        //opp betsize
+    }
 
-        //facingodds
+    private void getOppPreCall3betRange() {
+        //input:
+            //preCall3betGroup
+                //low
+                    //70% plus
+                    //suited connectors
+                //medium
+                    //50% plus
+                    //all suited connectors and suited onegappers and two-gappers
+                //high
+                    //35% plus
+                    //any suited
+    }
+    
+    private void getOppRaiseRangeYo() {
+        //input
 
         //opp aggroness
+            //low
+            //medium
+            //high
 
-        //river include ook misseddraws
+        //betsize
+            //small
+            //medium
+            //large
 
-        //basis:
-            //bovenste 40% combos voor value
-                //indien betsize > 100 && betsize < 200
-                    //bovenste 30% combos voor value
+        //------------
 
-                //indien betsize > 200 && betsize < 300
-                    //bovenste 25% combos voor value
+        //opp aggro low
+            //raisesize small
+                //value
+                    //vanaf 80%
 
-                //indien betsize > 300
-                    //bovenste 20% combos voor value
+                //draw
+                    //none
 
-                //-----
+                //air
+                    //none
 
-                //
+            //raisesize medium
+                //value
+                    //vanaf 87%
+
+                //draw
+                    //none
+
+                //air
+                    //none
+
+            //raisesize large
+                //value
+                    //vanaf 92
+
+                //draw
+                    //none
+
+                //air
+                    //none
+
+        //opp aggro medium
+            //raisesize small
+                //value
+                    //vanaf 75%
+
+                //draw
+                    //strong draws
+
+                //air
+                    //12% onder 50%
+
+            //raisesize medium
+                //value
+                    //vanaf 79%
+
+                //draw
+                    //strong draws
+
+                //air
+                    //10% onder 50%
+
+            //raisesize large
+                //value
+                    //vanaf 83%
+
+                //draw
+                    //strong draws
+
+                //air
+                    //8% onder 50%
 
 
+        //opp aggro high
+            //raisesize small
+                //value
+                    //vanaf 60%
 
-            //alle strong draws -> oosd, fd, gutshot
+                //draw
+                    //alle strong en medium draws
 
-            //alle medium draws -> oosd, fd, gutshot
+                //air
+                    //24% van onderste 50% combos
 
-            //18% air
+            //raisesize medium
+                //value
+                    //vanaf 70%
 
+                //draw
+                    //alle strong en medium draws
 
+                //air
+                    //21% van onderste 50% combos (non draw)
 
+            //raisesize large
+                //value
+                    //vanaf 78%
+
+                //draw
+                    //alle strong draws
+                    //50% van de medium draws
+
+                //air
+                    //18% van onderste 50% combos
 
     }
 
-    private void getOppRaiseRangeYo() {
+    private void getOppBetRangeYo(List<List<Card>> oppStartingRange, List<List<Card>> allCombosEquitySorted,
+                                  List<Card> knownGameCards, String oppAggroness, String oppBetsize) {
+        List<List<Card>> oppBetRange = new ArrayList<>();
 
-    }
+        if(oppAggroness.equals("low")) {
+            if(oppBetsize.equals("small")) {
+                //value
+                    //wat je hier kunt doen... alle 1326 combos sorteren
+                    //en dan... de bovenste 60% handhaven
+                    //en dan... van deze combos de combos houden die al in je range zaten...
 
-    private void getOppBetRangeYo() {
+                oppBetRange = allCombosEquitySorted.subList(0, 530);
+                oppBetRange.retainAll(oppStartingRange);
+                oppBetRange = removeCombosWithKnownCards(oppBetRange, knownGameCards);
+            } else if(oppBetsize.equals("medium")) {
+
+
+
+            } else if(oppBetsize.equals("larg")) {
+
+            }
+        }
+
+
+
         //input
 
         //opp aggroness
@@ -248,54 +540,66 @@ public class RangeConstructor {
 
         //opp aggro low
             //betsize small
+                //value
+                    //vanaf 60%
+
+                //draw
+                    //alle strong draws
+
+                //air
+                    //5% van onderste 50% combos
 
             //betsize medium
+                //value
+                    //bovenste 70%
+
+                //draw
+                    //strong fd en strong oosd
+
+                //air
+                    //4% van onderste 50% combos
 
             //betsize large
+                //value
+                    //vanaf 83%
+
+                //draw
+                    //50% strong fd en strong oosd
+
+                //air
+                    //2% van onderste 50% combos
 
 
         //opp aggro medium
             //betsize small
                 //value
-                    //bovenste 60% combos
-                    //18% van combos tussen 50 en 60
+                    //vanaf 53%
 
                 //draw
                     //alle strong draws
                     //alle medium draws
-
-                //missed strong draw river
-                    //50% missed draws
 
                 //air
                     //20% van onderste 50% combos
 
             //betsize medium
                 //value
-                    //bovenste 70% combos
-                    //18% van combos tussen 60 en 70
+                    //vanaf 63%
 
                 //draw
                     //alle strong draws
                     //50% medium draws
-
-                //missed strong draw river
-                    //45% missed draws
 
                 //air
                     //17% van onderste 50% combos
 
             //betsize large
                 //value
-                    //bovenste 80% combos
-                    //18% van combos tussen 70 en 80
+                    //bovenste 77% combos
 
                 //draw
                     //alle strong draws
                     //20% medium draws
-
-                //missed strong draw river
-                    //40% missed draws
 
                 //air
                     //15% van onderste 50% combos
@@ -303,10 +607,38 @@ public class RangeConstructor {
 
         //opp aggro high
             //betsize small
+                //value
+                    //vanaf 45%
+
+                //draw
+                    //alle strong draws
+                    //alle medium draws
+                    //alle weak draws
+
+                //air
+                    //33% van onderste 50% combos
 
             //betsize medium
+                //value
+                    //vanaf 55%
+
+                //draw
+                    //alle strong draws
+                    //alle medium draws
+
+                //air
+                    //28% van onderste 50% combos
 
             //betsize large
+                //value
+                    //vanaf 68%
+
+                //draw
+                    //alle strong draws
+                    //70% medium draws
+
+                //air
+                    //25% van onderste 50% combos
 
     }
 
@@ -618,6 +950,48 @@ public class RangeConstructor {
 
     }
 
+
+
+    private boolean comboIsSuitedConnector(List<Card> combo, int gapWith) {
+        boolean comboIsSuitedConnector = false;
+
+        if(combo.get(0).getSuit() == combo.get(1).getSuit()) {
+            int rankCard1ToUse = combo.get(0).getRank();
+            int rankCard2ToUse = combo.get(1).getRank();
+
+            if(rankCard1ToUse > rankCard2ToUse) {
+                if(rankCard1ToUse - rankCard2ToUse == gapWith) {
+                    comboIsSuitedConnector = true;
+                }
+            } else {
+                if(rankCard2ToUse - rankCard1ToUse == gapWith) {
+                    comboIsSuitedConnector = true;
+                }
+            }
+
+
+            if(!comboIsSuitedConnector &&
+                    (combo.get(0).getRank() == 14 || combo.get(1).getRank() == 14)) {
+                if(combo.get(0).getRank() == 14) {
+                    rankCard1ToUse = 1;
+                } else {
+                    rankCard2ToUse = 1;
+                }
+
+                if(rankCard1ToUse > rankCard2ToUse) {
+                    if(rankCard1ToUse - rankCard2ToUse == gapWith) {
+                        comboIsSuitedConnector = true;
+                    }
+                } else {
+                    if(rankCard2ToUse - rankCard1ToUse == gapWith) {
+                        comboIsSuitedConnector = true;
+                    }
+                }
+            }
+        }
+
+        return comboIsSuitedConnector;
+    }
 
 
 }
