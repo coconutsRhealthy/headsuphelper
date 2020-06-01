6bac26c09bed1f7066044045837dd0f4d55c396f
LennartMac <lennartpopma@hotmail.com>
2020-06-01 20:16:50 +0200

    Increase size of testmethod in BotActionBuilder

--- src/main/java/com/lennart/model/action/actionbuilders/ai/equityrange/BotActionBuilder.java
+++ src/main/java/com/lennart/model/action/actionbuilders/ai/equityrange/BotActionBuilder.java
@@ -28,34 +28,52 @@ public class BotActionBuilder {
         ContinuousTable continuousTable = new ContinuousTable();
         GameVariables gameVariables = new GameVariables();
 
-        gameVariables.setBotHoleCards(Arrays.asList(new Card(6, 'd'), new Card(7, 'd')));
-        gameVariables.setBoard(Arrays.asList(new Card(2, 's'), new Card(12, 'h'), new Card(14, 'd')));
-        gameVariables.setOpponentAction("empty");
-        gameVariables.setBotIsButton(false);
-        gameVariables.setBotStack(840);
-        gameVariables.setOpponentStack(840);
-        gameVariables.setBotBetSize(0);
-        gameVariables.setOpponentBetSize(0);
+        //////1
+        gameVariables.setBotHoleCards(Arrays.asList(new Card(5, 's'), new Card(6, 'h')));
+        gameVariables.setBoard(new ArrayList<>());
+        gameVariables.setOpponentAction("bet");
+        gameVariables.setBotIsButton(true);
+        gameVariables.setBotStack(990);
+        gameVariables.setOpponentStack(980);
+        gameVariables.setBotBetSize(10);
+        gameVariables.setOpponentBetSize(20);
         gameVariables.setBigBlind(20);
-        gameVariables.setPot(320);
+        gameVariables.setPot(0);
         gameVariables.setOpponentName("TestNewEquityStyle");
 
-        PlayerActionRound playerActionRound1_1 = new PlayerActionRound("opponent", new ArrayList<>(), gameVariables.getBigBlind(), gameVariables.getBigBlind() / 2, "preflop", "postSB");
-        PlayerActionRound playerActionRound1_2 = new PlayerActionRound("bot", new ArrayList<>(), gameVariables.getBigBlind(), gameVariables.getBigBlind() / 2, "preflop", "bet");
-        PlayerActionRound playerActionRound1_3 = new PlayerActionRound("opponent", new ArrayList<>(), gameVariables.getBigBlind(), 43, "preflop", "raise");
+        PlayerActionRound playerActionRound1_1 = new PlayerActionRound("bot", new ArrayList<>(), gameVariables.getBigBlind() / 2, gameVariables.getBigBlind(), "preflop", "postSB");
+        PlayerActionRound playerActionRound1_2 = new PlayerActionRound("opponent", new ArrayList<>(), gameVariables.getBigBlind() / 2, gameVariables.getBigBlind(), "preflop", "bet");
+        PlayerActionRound playerActionRound1_3 = new PlayerActionRound("bot", new ArrayList<>(), 42, gameVariables.getBigBlind(), "preflop", "raise");
+        PlayerActionRound playerActionRound1_4 = new PlayerActionRound("opponent", new ArrayList<>(), 42, gameVariables.getBigBlind(), "preflop", "call");
 
-        ActionRequest actionRequest1 = new ActionRequest(gameVariables.getAllActionRequestsOfHand(), 63, gameVariables.getBoard(), gameVariables.isBotIsButton(), gameVariables.getBigBlind());
+        ActionRequest actionRequest1 = new ActionRequest(gameVariables.getAllActionRequestsOfHand(), 10, gameVariables.getBoard(), gameVariables.isBotIsButton(), gameVariables.getBigBlind());
 
         actionRequest1.getActionsSinceLastRequest().add(playerActionRound1_1);
         actionRequest1.getActionsSinceLastRequest().add(playerActionRound1_2);
         actionRequest1.getActionsSinceLastRequest().add(playerActionRound1_3);
+        actionRequest1.getActionsSinceLastRequest().add(playerActionRound1_4);
 
         gameVariables.getAllActionRequestsOfHand().add(actionRequest1);
 
-        PlayerActionRound playerActionRound2_1 = new PlayerActionRound("bot", new ArrayList<>(), 160, 43, "preflop", "raise");
-        PlayerActionRound playerActionRound2_2 = new PlayerActionRound("opponent", new ArrayList<>(), 160, 160, "preflop", "call");
+        RangeConstructor rangeConstructor = new RangeConstructor();
+        new OpponentRangeSetter(rangeConstructor, new InputProvider()).setOpponentRange(continuousTable, gameVariables);
 
-        ActionRequest actionRequest2 = new ActionRequest(gameVariables.getAllActionRequestsOfHand(), 320, gameVariables.getBoard(), gameVariables.isBotIsButton(), gameVariables.getBigBlind());
+
+
+
+        //////2
+        gameVariables.setBoard(Arrays.asList(new Card(8, 'd'), new Card(12, 'd'), new Card(11, 'c')));
+        gameVariables.setOpponentAction("check");
+        gameVariables.setBotStack(958);
+        gameVariables.setOpponentStack(958);
+        gameVariables.setBotBetSize(0);
+        gameVariables.setOpponentBetSize(0);
+        gameVariables.setPot(84);
+
+        PlayerActionRound playerActionRound2_1 = new PlayerActionRound("bot", gameVariables.getBoard(), 0, 0, "flop", "check");
+        PlayerActionRound playerActionRound2_2 = new PlayerActionRound("opponent", gameVariables.getBoard(), 0, 0, "flop", "check");
+
+        ActionRequest actionRequest2 = new ActionRequest(gameVariables.getAllActionRequestsOfHand(), 84, gameVariables.getBoard(), gameVariables.isBotIsButton(), gameVariables.getBigBlind());
 
         actionRequest2.getActionsSinceLastRequest().add(playerActionRound2_1);
         actionRequest2.getActionsSinceLastRequest().add(playerActionRound2_2);
@@ -66,17 +84,98 @@ public class BotActionBuilder {
         DbSaveRaw dbSaveRaw = new DbSaveRaw();
         dbSaveRaw.setBoard("");
         dbSaveRaw.setBotAction("raise");
-        dbSaveRaw.setSizing(160);
+        dbSaveRaw.setSizing(42);
         dbSaveList.add(dbSaveRaw);
         continuousTable.setDbSaveList(dbSaveList);
 
-        RangeConstructor rangeConstructor = new RangeConstructor();
+        RangeConstructor rangeConstructor2 = new RangeConstructor();
+        new OpponentRangeSetter(rangeConstructor2, new InputProvider()).setOpponentRange(continuousTable, gameVariables);
 
-        new OpponentRangeSetter(rangeConstructor, new InputProvider()).setOpponentRange(continuousTable, gameVariables);
 
-        String action = getAction(continuousTable, gameVariables, rangeConstructor);
 
-        System.out.println(action);
+
+
+        //////3
+        gameVariables.setBoard(Arrays.asList(new Card(8, 'd'), new Card(12, 'd'), new Card(11, 'c'), new Card(8, 'c')));
+        gameVariables.setOpponentAction("check");
+        gameVariables.setBotStack(958);
+        gameVariables.setOpponentStack(958);
+        gameVariables.setBotBetSize(0);
+        gameVariables.setOpponentBetSize(0);
+        gameVariables.setPot(84);
+
+        PlayerActionRound playerActionRound3_1 = new PlayerActionRound("bot", gameVariables.getBoard(), 0, 0, "turn", "check");
+        PlayerActionRound playerActionRound3_2 = new PlayerActionRound("opponent", gameVariables.getBoard(), 0, 0, "turn", "check");
+
+        ActionRequest actionRequest3 = new ActionRequest(gameVariables.getAllActionRequestsOfHand(), 84, gameVariables.getBoard(), gameVariables.isBotIsButton(), gameVariables.getBigBlind());
+
+        actionRequest3.getActionsSinceLastRequest().add(playerActionRound3_1);
+        actionRequest3.getActionsSinceLastRequest().add(playerActionRound3_2);
+
+        gameVariables.getAllActionRequestsOfHand().add(actionRequest3);
+
+        DbSaveRaw dbSaveRaw2 = new DbSaveRaw();
+        dbSaveRaw2.setBoard("8d12d11c");
+        dbSaveRaw2.setBotAction("check");
+        dbSaveRaw2.setSizing(0);
+        continuousTable.getDbSaveList().add(dbSaveRaw2);
+
+        RangeConstructor rangeConstructor3 = new RangeConstructor();
+        new OpponentRangeSetter(rangeConstructor3, new InputProvider()).setOpponentRange(continuousTable, gameVariables);
+
+
+
+
+        ////////4
+        gameVariables.setBoard(Arrays.asList(new Card(8, 'd'), new Card(12, 'd'), new Card(11, 'c'), new Card(8, 'c'), new Card(2, 's')));
+        gameVariables.setOpponentAction("check");
+        gameVariables.setBotStack(958);
+        gameVariables.setOpponentStack(958);
+        gameVariables.setBotBetSize(0);
+        gameVariables.setOpponentBetSize(0);
+        gameVariables.setPot(84);
+
+        DbSaveRaw dbSaveRaw3 = new DbSaveRaw();
+        dbSaveRaw3.setBoard("8d12d11c8c");
+        dbSaveRaw3.setBotAction("check");
+        dbSaveRaw3.setSizing(0);
+        continuousTable.getDbSaveList().add(dbSaveRaw3);
+
+        RangeConstructor rangeConstructor4 = new RangeConstructor();
+        new OpponentRangeSetter(rangeConstructor4, new InputProvider()).setOpponentRange(continuousTable, gameVariables);
+
+        String action = getAction(continuousTable, gameVariables, rangeConstructor4);
+
+        System.out.println("ACTION: " + action);
+
+//
+//
+//        PlayerActionRound playerActionRound3_1 = new PlayerActionRound("opponent", Arrays.asList(new Card(8, 'd'), new Card(12, 'd'), new Card(11, 'c'), new Card(8, 'c')), 0, 0, "turn", "check");
+//        PlayerActionRound playerActionRound3_2 = new PlayerActionRound("bot", Arrays.asList(new Card(8, 'd'), new Card(12, 'd'), new Card(11, 'c'), new Card(8, 'c')), 0, 0, "turn", "check");
+//
+//        ActionRequest actionRequest3 = new ActionRequest(gameVariables.getAllActionRequestsOfHand(), 84, gameVariables.getBoard(), gameVariables.isBotIsButton(), gameVariables.getBigBlind());
+//
+//        actionRequest2.getActionsSinceLastRequest().add(playerActionRound3_1);
+//        actionRequest2.getActionsSinceLastRequest().add(playerActionRound3_2);
+//
+//        gameVariables.getAllActionRequestsOfHand().add(actionRequest2);
+//        new OpponentRangeSetter(rangeConstructor, new InputProvider()).setOpponentRange(continuousTable, gameVariables);
+//
+//        List<DbSave> dbSaveList = new ArrayList<>();
+//        DbSaveRaw dbSaveRaw = new DbSaveRaw();
+//        dbSaveRaw.setBoard("");
+//        dbSaveRaw.setBotAction("raise");
+//        dbSaveRaw.setSizing(160);
+//        dbSaveList.add(dbSaveRaw);
+//        continuousTable.setDbSaveList(dbSaveList);
+//
+//
+//
+//        new OpponentRangeSetter(rangeConstructor, new InputProvider()).setOpponentRange(continuousTable, gameVariables);
+//
+//        String action = getAction(continuousTable, gameVariables, rangeConstructor);
+//
+//        System.out.println(action);
     }
 
     public String getAction(ContinuousTable continuousTable, GameVariables gameVariables, RangeConstructor rangeConstructor) {
